5415d7417a0471cae5566e9632ab800d2ca0886d
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-10-06 23:06:20 +0900

    Update training_client.py

--- self_driving_car/training/raspberry_pi/training_client.py
+++ self_driving_car/training/raspberry_pi/training_client.py
@@ -8,8 +8,7 @@ PORT = 15946
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP, PORT))
-steering_value = b''
-cnt = 0
+
 try:
     cap = cv2.VideoCapture('test.mp4')
     while True:
@@ -18,9 +17,6 @@ try:
         data = pickle.dumps(image)
         message_size = struct.pack('L', len(data))
         client_socket.sendall(message_size+data)
-        steering_value = client_socket.recv(10)
-        # print(struct.unpack('B', b)[0])
-
 
 except Exception as e:
     print("General error", str(e))
