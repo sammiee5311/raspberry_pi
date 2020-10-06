86f74cd941b2bf7a5a920185c03f7159cb7137ed
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-10-06 23:20:51 +0900

    Update client.py

--- self_driving_car/client.py
+++ self_driving_car/client.py
@@ -1,30 +1,58 @@
 import socket
-import cv2
-import struct
 import pickle
+import cv2
+import numpy as np
+import struct
+from keras.models import load_model
+
 
 IP = '127.0.0.1'
 PORT = 15946
+model = load_model('my_model.h5')
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, PORT))
-steering_value = b''
-cnt = 0
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+server_socket.listen()
+print("waiting for connection")
+
+connection, address = server_socket.accept()
+
 try:
-    cap = cv2.VideoCapture('test.mp4')
+    bytes = b''
+    bytes += connection.recv(1024)
+    payload_size = struct.calcsize("L")
     while True:
-        cnt += 1
-        _, image = cap.read()
-        data = pickle.dumps(image)
-        message_size = struct.pack('L', len(data))
-        client_socket.sendall(message_size+data)
-        steering_value = client_socket.recv(10)
-        # print(struct.unpack('B', b)[0])
+        while len(bytes) < payload_size:
+            bytes += connection.recv(1024)
+        packed_msg_size = bytes[:payload_size]
+        bytes = bytes[payload_size:]
+        msg_size = struct.unpack('L', packed_msg_size)[0]
 
+        while len(bytes) < msg_size:
+            bytes += connection.recv(1024)
+        frame_data = bytes[:msg_size]
+        bytes = bytes[msg_size:]
 
-except Exception as e:
-    print("General error", str(e))
-    client_socket.close()
+        frame = pickle.loads(frame_data)
 
+        image = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        height,width = image.shape
+        roi = image[height // 2:height, :]
+        roi /= 2555
+        roi = np.array([roi])
+        steering_value = float(model.predict(roi))
+        connection.send(struct.pack('B', steering_value))
+        cv2.imshow('frame', roi)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+    try:
+        pass
+    except IOError as e:
+        print(e)
 
-
+finally:
+    connection.close()
+    server_socket.close()
