714fc76a653da92d5e2d855b937419c275cf82da
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-10-06 23:12:42 +0900

    Add files via upload

---
+++ self_driving_car/training/server.py
@@ -0,0 +1,63 @@
+import socket
+import pickle
+import cv2
+import numpy as np
+import struct
+
+IP = '127.0.0.1'
+PORT = 15946
+input_size = 480*320
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+server_socket.listen()
+print("waiting for connection")
+
+connection, address = server_socket.accept()
+
+cnt = 0
+
+X = []
+
+try:
+    bytes = b''
+    bytes += connection.recv(1024)
+    payload_size = struct.calcsize("L")
+    while True:
+        while len(bytes) < payload_size:
+            bytes += connection.recv(1024)
+        packed_msg_size = bytes[:payload_size]
+        bytes = bytes[payload_size:]
+        msg_size = struct.unpack('L', packed_msg_size)[0]
+
+        while len(bytes) < msg_size:
+            bytes += connection.recv(1024)
+        frame_data = bytes[:msg_size]
+        bytes = bytes[msg_size:]
+
+        frame = pickle.loads(frame_data)
+        connection.send(struct.pack('B', cnt))
+
+        image = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        height,width = image.shape
+        roi = image[height // 2:height, :]
+        # tmp = roi.reshape(1, height // 2 * width).astype(np.float32)
+        # X = np.vstack((X, tmp))
+        cv2.imshow('frame', roi)
+        X.append(roi)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+    try:
+        for img in X:
+            cnt += 1
+            cv2.imwrite(str(cnt)+'.jpg')
+        # np.savez('save.npz', train=X)
+    except IOError as e:
+        print(e)
+
+finally:
+    connection.close()
+    server_socket.close()
