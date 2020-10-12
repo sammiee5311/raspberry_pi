636969b1774cb90e77ff83458795f1335feb7882
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-10-12 22:09:23 +0900

    Add files via upload

---
+++ self_driving_car/training/main.py
@@ -0,0 +1,50 @@
+from Motor_module import Motor
+import Controller as ct
+import pandas as pd
+import cv2
+import os
+import numpy as np
+from PIL import Image
+
+motor = Motor(5, 27, 17, 6, 23, 22)
+path = './Images/'
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    X = []
+    y = []
+    cnt = 0
+    maxThrottle = 0.25
+    while True:
+        _, image = cap.read()
+        # image_cp = image.copy()
+        # image_cp = cv2.cvtColor(image_cp,cv2.COLOR_RGB2GRAY)
+        # height, width = image_cp.shape
+        # roi = image_cp[int(height/2):height,:]
+        # cv2.imshow('img',image)
+        ct_val = ct.getJS()
+        steering = ct_val['axis1']*0.4
+        throttle = ct_val['o']*maxThrottle
+        motor.move(throttle,-steering)
+        X.append(image)
+        y.append(str(round(steering,5)))
+        cnt += 1
+        print("steering : ", steering, "throttle : ", throttle)
+        if ct_val['x']:
+           break
+        cv2.waitKey(1)
+
+    for i in range(len(X)):
+       img = Image.fromarray(X[i])
+       img.save(path+str(i)+'.jpg')
+
+    df = pd.DataFrame({'steering' : y})
+    df.to_csv('save.csv')
+
+
+if __name__ == '__main__':
+    while True:
+        main()
+        break
+
