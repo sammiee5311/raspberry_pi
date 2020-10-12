8c2a5df8735d53407d8e186a1d78bf5df80db8bb
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-10-12 22:12:35 +0900

    Update client.py

--- self_driving_car/raspberry_pi/client.py
+++ self_driving_car/raspberry_pi/client.py
@@ -4,25 +4,25 @@ import struct
 import pickle
 import numpy as np
  
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.0.18', 8485))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('192.168.0.18', 8485))
 steering_value = b''
  
-cam = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
  
-cam.set(3, 480);
-cam.set(4, 240);
+cap.set(3, 480);
+cap.set(4, 240);
  
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
  
 while True:
-    ret, frame = cam.read()
+    ret, frame = cap.read()
     result, frame = cv2.imencode('.jpg', frame, encode_param)
     data = np.array(frame)
     stringData = data.tostring()
  
-    s.sendall((str(len(stringData))).encode().ljust(16) + stringData)
-    steering_value = s.recv(10)
+    client.sendall((str(len(stringData))).encode().ljust(16) + stringData)
+    steering_value = client.recv(10)
     print(struct.unpack('B',steering_value)[0])
  
-cam.release()
+cap.release()
