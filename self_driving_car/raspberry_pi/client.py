b3acaa5dc66f085ee7c7c0602431fa2fe621d4e2
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-11-21 12:16:17 +0900

    Update client.py

--- self_driving_car/raspberry_pi/client.py
+++ self_driving_car/raspberry_pi/client.py
@@ -3,26 +3,28 @@ import cv2
 import struct
 import pickle
 import numpy as np
+from motor_module import Motor
+
+motor = Motor(5, 27, 17, 6, 23, 22)
  
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('IP', PORT))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('192.168.0.18', 9484))
 steering_value = b''
  
-cap = cv2.VideoCapture(0)
- 
-# cap.set(3, 480);
-# cap.set(4, 240);
+cam = cv2.VideoCapture(0)
  
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 100]
  
 while True:
-    ret, frame = cap.read()
+    ret, frame = cam.read()
     result, frame = cv2.imencode('.jpg', frame, encode_param)
     data = np.array(frame)
     stringData = data.tostring()
  
-    client.sendall((str(len(stringData))).encode().ljust(16) + stringData)
-    steering_value = client.recv(10)
-    steering_value = struct.unpack('f',steering_value)[0])
+    s.sendall((str(len(stringData))).encode().ljust(16) + stringData)
+    steering_value = s.recv(10)
+    steering_value = struct.unpack('f',steering_value)[0]
+    motor.move(0.12,steering_value)
  
-cap.release()
+cam.release()
+
