b280bc5eb0a47e2fb7776cc86f9fe451a989d3e2
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-11-21 11:59:26 +0900

    Update main.py

--- self_driving_car/training/main.py
+++ self_driving_car/training/main.py
@@ -4,6 +4,7 @@ import cv2
 import keras
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
 from keras import models
 from keras import layers
 
@@ -16,27 +17,51 @@ df = pd.read_csv("save.csv")
 
 for i in range(len(df)):
     value = df.iloc[i]
-    img = cv2.imread(path+str(int(value[1]))+'.jpg')
-    images.append(img)
-    steering.append(value[2])
+    img = cv2.imread(path + str(int(value[1])) + '.jpg')
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img,  (3, 3), 0)
+    h, w, _ = img.shape
+    roi = img[int(h / 2):h, :, :]
+    if np.random.rand() < 0.5:
+        roi = cv2.flip(roi, 1)
+        val = -value[2])
+    roi = roi / 255.
+    images.append(roi)
+    steering.append(val)
+    cnt[val] += 1
+
+for i,v in enumerate(steering):
+    if v == -3:
+        values[i][0] = 1
+    elif v == -2:
+        values[i][1] = 1
+    elif v == -1:
+        values[i][2] = 1
+    elif v == 0:
+        values[i][3] = 1
+    elif v == 1:
+        values[i][4] = 1
+    elif v == 2:
+        values[i][5] = 1
+    elif v == 3:
+        values[i][6] = 1
+
 
 images = np.asarray(images)
-steering = np.asarray(steering)
+values = np.asarray(values)
+images = images.astype('float32')
 
 
-X_train, X_test, y_train, y_test = train_test_split(images,steering,test_size=0.25, random_state=1)
-X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=0.25, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(images, values, test_size=0.2, random_state=1)
+X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
 
 
 train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    zoom_range=0.2,
+    rescale=1. / 255
 )
 
 test_datagen = ImageDataGenerator(
-    rescale=1./255
+    rescale=1. / 255
 )
 
 model = models.Sequential()
@@ -50,15 +75,15 @@ model.add(layers.Flatten())
 model.add(layers.Dense(units=100, activation='elu'))
 model.add(layers.Dense(units=50, activation='elu'))
 model.add(layers.Dense(units=10, activation='elu'))
-model.add(layers.Dense(units=1))
+model.add(layers.Dense(units=7, activation='softmax'))
 
-model.compile(optimizer='adam',
+model.compile(Adam(lr=0.0001),
               loss='categorical_crossentropy',
               metrics=['acc'])
 
 callbacks = [
     keras.callbacks.ModelCheckpoint(
-        filepath='my_model.h5',
+        filepath='model2.h5',
         monitor='val_loss',
         save_best_only=True
     )
@@ -79,6 +104,7 @@ model.fit(
     train_generator,
     epochs=20,
     validation_data=validation_generator,
-    shuffle=1,
     callbacks=callbacks
 )
+
+model.save('model.h5')
