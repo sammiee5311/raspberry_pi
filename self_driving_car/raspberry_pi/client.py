772a6fb28d9d6bd49cef4671ec4e881028cd545a
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-11-28 22:11:33 +0900

    Update client.py

--- self_driving_car/raspberry_pi/client.py
+++ self_driving_car/raspberry_pi/client.py
@@ -1,20 +1,29 @@
 import socket
+import RPi.GPIO as GPIO
 import cv2
 import struct
-import pickle
 import numpy as np
-from motor_module import Motor
 
-motor = Motor(5, 27, 17, 6, 23, 22)
-IP =  
+########################################
+from motor_module import Motor
+from ultrasonic_module import Ultrasonic
+########################################
+
+GPIO.cleanup()
+IP = ''
 PORT = 
 
+motor = Motor(5, 27, 17, 6, 23, 22)
+dis = Ultrasonic()
+dis.set_GPIO()
+ 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('IP', PORT))
+s.connect((IP, PORT))
+
 steering_value = b''
  
 cam = cv2.VideoCapture(0)
- 
+
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 100]
  
 while True:
@@ -25,8 +34,12 @@ while True:
  
     s.sendall((str(len(stringData))).encode().ljust(16) + stringData)
     steering_value = s.recv(10)
-    steering_value = struct.unpack('f',steering_value)[0]
-    motor.move(0.12,steering_value)
- 
-cam.release()
+    steering_value = struct.unpack('f',steering_value)[0
+                                                       
+    distance = dis.get_distance()
+    if distance <= 10:
+        motor.move(0,0)
+    else:
+        motor.move(0.11,steering_value)
 
+cam.release()
