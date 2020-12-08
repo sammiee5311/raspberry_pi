aa81e4a335495ae1bc75a9eda2cff1d486801a35
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-12-08 22:43:56 +0900

    Add files via upload

---
+++ self_driving_car/test/computer/yolo_module.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+
+
+class Object_Detection:
+    def __init__(self):
+        self.net = cv2.dnn.readNet("./yolo_file/yolov3.weights", "./yolo_file/yolov3.cfg")
+        self.layer_names = self.net.getLayerNames()
+        self.output_layers = [self.layer_names[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
+
+    def detect_object(self, img):
+        x, y, w, h = -1, -1, -1, -1
+        # img = cv2.resize(img, None, fx=0.4, fy=0.4)
+        height, width, _ = img.shape
+
+        blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+        self.net.setInput(blob)
+        outs = self.net.forward(self.output_layers)
+
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.3:
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+
+                    return x, y, w, h
+
+        # indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        #
+        # for i in range(len(boxes)):
+        #     if i in indexes:
+        #         x, y, w, h = boxes[i]
+        #         cv2.rectangle(img, (x, y), (x + w, y + h), (0,255,0), 2)
+
+        return x, y, w, h
+
