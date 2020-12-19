6d8ec3f100ad7e36b233068a4479255e00cf62aa
Sam.An <56215891+sammiee5311@users.noreply.github.com>
2020-12-19 20:54:46 +0900

    Update training_client.py

--- self_driving_car/training/raspberry_pi/training_client.py
+++ self_driving_car/training/raspberry_pi/training_client.py
@@ -12,11 +12,10 @@ client_socket.connect((IP, PORT))
 try:
     cap = cv2.VideoCapture(0)
     while True:
-        cnt += 1
         _, image = cap.read()
         data = pickle.dumps(image)
-        message_size = struct.pack('L', len(data))
-        client_socket.sendall(message_size+data)
+        img_data = struct.pack('L', len(data))
+        client_socket.sendall(img_data+data)
 
 except Exception as e:
     print("General error", str(e))
